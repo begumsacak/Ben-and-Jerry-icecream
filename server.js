a4816608276b09bc4aeeba946745af60244dcd21
begumsacak <63175082+begumsacak@users.noreply.github.com>
2020-07-28 21:24:07 -0500

    made index and flavor templates

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 
 const app = express()
-const PORT = 8080
+const PORT = 3000
 
 //handlebar documentation
 app.engine('handlebars', exphbs());
@@ -17,13 +17,19 @@ var icecreams = [
     { name: "pistachio", price: 11, awesomeness: 15 }
 ];
 
-app.get(`/icecream/:name`), (req,res) => {
+app.get(`/`, (req, res) => {
+    res.render("index")
+})
+
+app.get(`/icecream/:name`, (req, res) => {
+    const targetFlavor = req.params.name
+    const found = icecreams.find(flavor => flavor.name === targetFlavor)
+    res.render("flavor", found)
+})
+
+app.get(`icecreams`, (req, res) => {
 
 })
 
-app.get(`icecreams`), (req, res) => {
 
-})
-
-
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`)
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
